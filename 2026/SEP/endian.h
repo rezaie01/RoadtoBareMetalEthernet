7f417eb21a9b2b33f92822542db1dbf0ba04b996
rezaie01 <rezaie.de.01@gmail.com>
2026-09-12 16:05:15 +0200

    fix: add missing header guards in endian.h and types.h

--- 2026/SEP/endian.h
+++ 2026/SEP/endian.h
@@ -1,5 +1,12 @@
+#ifndef __PARSE_IPV4_HEADER_H__
+
+#define __PARSE_IPV4_HEADER_H__
+
+
 #include "types.h"
 
 bool is_host_LE();
 u16 u16_to_BE(u16 n);
 u32 u32_to_BE(u32 n);
+
+#endif
--- 2026/SEP/types.h
+++ 2026/SEP/types.h
@@ -1,5 +1,10 @@
+#ifndef ___MEIN_TYPES_H__
+#define ___MEIN_TYPES_H__
+
 #include <stdint.h>
 
 typedef unsigned char u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+
+#endif
