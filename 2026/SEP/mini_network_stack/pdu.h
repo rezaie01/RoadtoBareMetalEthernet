61b200018dd3e3b0879723941406e7989e22f0c2
rezaie01 <rezaie.de.01@gmail.com>
2026-09-13 19:52:05 +0200

    refactor: update tPDU structure definition for clarity and consistency

--- 2026/SEP/mini_network_stack/pdu.h
+++ 2026/SEP/mini_network_stack/pdu.h
@@ -3,11 +3,18 @@
 #define __MNS_PDU_H__ 
 #include "../types.h"
 
-typedef struct tPDU {
+struct tPDU;
+
+typedef struct tPDU tPDU;
+
+struct tPDU {
     void *header;
     void *footer;
-    
-    u8 data[];
-} tPDU;
+    u8* data;
+    u32 total_len;
+    u32 payload_len;
+
+    char* (*get_repr_str)(tPDU* self);
+};
 
 #endif
