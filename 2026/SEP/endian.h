1a29b86a152d3fd52aed19eea66920746f23daae
rezaie01 <rezaie.de.01@gmail.com>
2026-09-13 22:34:37 +0200

    fix: correct header guard definition in endian.h

--- 2026/SEP/endian.h
+++ 2026/SEP/endian.h
@@ -1,14 +1,13 @@
-#ifndef __PARSE_IPV4_HEADER_H__
+#ifndef __MSN_ENDIAN_H__
 
-#define __PARSE_IPV4_HEADER_H__
-
+#define __MSN_ENDIAN_H__
 
 #include "types.h"
 
 bool is_host_LE();
 u16 u16_to_BE(u16 n);
 u32 u32_to_BE(u32 n);
-u16 bytes_to_hostu16(u8 b1, u8 b0);
 
+u16 bytes_to_hostu16(u8 b1, u8 b0);
 u32 bytes_to_hostu32(u8 b3, u8 b2, u8 b1, u8 b0);
 #endif
