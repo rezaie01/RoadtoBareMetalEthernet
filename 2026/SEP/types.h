813a96dec5e095dd90135d64120efd18d5b3009c
rezaie01 <rezaie.de.01@gmail.com>
2026-09-14 17:22:36 +0200

    feat: add EtherType enumeration for Ethernet protocol types

--- 2026/SEP/types.h
+++ 2026/SEP/types.h
@@ -7,4 +7,10 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 
+typedef enum EtherType {
+    ARP = 0x0806,
+    IPv4 = 0x0800,
+    IPv6 = 0x86DD,
+    WoN = 0x0842,
+} EtherType;
 #endif
