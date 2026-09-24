b041be482e57d331e721a1de7cb61bcaa515c567
rezaie01 <rezaie.de.01@gmail.com>
2026-09-24 17:03:41 +0200

    feat: - implement command-line-argument handler - implement netcalculator given a routing prefix as input

---
+++ src/netcalc.c
@@ -0,0 +1,61 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "netcalc.h"
+#include "mini_network_stack/shared.h"
+#include "endian.h"
+
+int handle_args(int argc, char *argv[])
+{
+    if (argc > 1)
+    {
+        // Routing prefix as an array
+        u8 *arr = argc == 2 ? str_t_routingprefixv4(argv[1]) : nullptr;
+        // argc 2, and argv[1] CIDR
+        if (arr)
+        {
+            u8 prfx = arr[4];
+
+            if (prfx > 32)
+                exit(1);
+
+            u8 prfx_diff = 32 - prfx; // TODO: watch out for prefix edge cases 32, 31, 0, 1
+            u32 ip_as_num = bytes_to_hostu32(arr[0], arr[1], arr[2], arr[3]);
+
+            u32 subnetz_maske = (u32)(pow(2, 32) - pow(2, prfx_diff));
+            u32 wildcard_maske = (u32)(pow(2, prfx_diff) - 1);
+            u32 netzwerk_id = ip_as_num & subnetz_maske;
+            u32 broadcast_ip = ip_as_num | wildcard_maske;
+            u32 min_ip = netzwerk_id + 1;
+            u32 max_ip = broadcast_ip - 1;
+            u32 usable_hst_c = prfx_diff < 2 ? 0 : (u32)(pow(2, prfx_diff)) - 2;
+
+            printf(format_str("IP-Adresse: %s\n", bytes_to_ipv4_address_str(arr, "")));
+            printf("Präfix: /%d\n", prfx);
+            printf(format_str("Subnetzmaske: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(subnetz_maske), "")));
+            printf(format_str("wildcardmaske: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(wildcard_maske), "")));
+            printf(format_str("Netzwerk ID: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(netzwerk_id), "")));
+            printf(format_str("Broadcast IP: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(broadcast_ip), "")));
+
+            printf(format_str("MinIP: %s\n", prfx_diff < 2 ? "NA" : bytes_to_ipv4_address_str(u32_to_bytes(min_ip), "")));
+            printf(format_str("MaxIP: %s\n", prfx_diff < 2 ? "NA" : bytes_to_ipv4_address_str(u32_to_bytes(max_ip), "")));
+            printf(format_str("Anzahl des Hosts: %u\n", usable_hst_c));
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+u8 *str_t_routingprefixv4(char *ipv4str)
+{
+    u8 *ip_arr = malloc(5 * sizeof(u8));
+
+    int dummy = -1;
+    // überprüfe ob das String formatlich korrekt ist.
+
+    int i = sscanf(ipv4str, "%u.%u.%u.%u/%u%d", ip_arr, ip_arr + 1, ip_arr + 2, ip_arr + 3, ip_arr + 4, &dummy);
+
+    return i == 5 ? ip_arr : nullptr;
+}
---
+++ src/netcalc.h
@@ -0,0 +1,13 @@
+#ifndef __NETCALC_H__
+#define __NETCALC_H__
+
+#include "types.h"
+
+// returns an array containing 4 octals of an ip, plus the prefix {0xxx,0xxx,0xxx,0xxx, prefix}
+int handle_args(int argc, char* argv[]);
+
+u8 *str_t_routingprefixv4(char *ipv4str);
+
+
+
+#endif
