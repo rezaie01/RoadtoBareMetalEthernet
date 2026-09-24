867d6eaefdfdf96e25b120eeeec1e2ad1e9e8ff0
rezaie01 <rezaie.de.01@gmail.com>
2026-09-24 17:42:10 +0200

    feat: replace pow function with intpowof2 for subnet mask calculations

--- src/netcalc.c
+++ src/netcalc.c
@@ -7,6 +7,11 @@
 #include "mini_network_stack/shared.h"
 #include "endian.h"
 
+u64 intpowof2(u8 n)
+{
+    return (u64)1 << (n);
+}
+
 int handle_args(int argc, char *argv[])
 {
     if (argc > 1)
@@ -24,19 +29,20 @@ int handle_args(int argc, char *argv[])
             u8 prfx_diff = 32 - prfx;
             u32 ip_as_num = bytes_to_hostu32(arr[0], arr[1], arr[2], arr[3]);
 
-            u32 subnetz_maske = (u32)(pow(2, 32) - pow(2, prfx_diff));
-            u32 wildcard_maske = (u32)(pow(2, prfx_diff) - 1);
+            printf("%d\n", prfx_diff);
+            u32 subnetz_maske = intpowof2(32) - intpowof2(prfx_diff);
+            u32 wildcard_maske = intpowof2(prfx_diff) - 1;
             u32 netzwerk_id = ip_as_num & subnetz_maske;
             u32 broadcast_ip = ip_as_num | wildcard_maske;
             u32 min_ip = netzwerk_id + 1;
             u32 max_ip = broadcast_ip - 1;
-            u32 usable_hst_c = prfx_diff < 2 ? 0 : (u32)(pow(2, prfx_diff)) - 2; // edge-case: /32 /31 handeled
+            u32 usable_hst_c = prfx_diff < 2 ? 0 : intpowof2(prfx_diff) - 2; // edge-case: /32 /31 handeled
 
             printf(format_str("IP-Adresse: %s\n", bytes_to_ipv4_address_str(arr, "")));
             printf("Präfix: /%d\n", prfx);
             printf(format_str("Subnetzmaske: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(subnetz_maske), "")));
             printf(format_str("wildcardmaske: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(wildcard_maske), "")));
-            printf(format_str("Netzwerk ID: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(netzwerk_id), "")));
+            printf(format_str("Netzwerk ID: %s/%d\n", bytes_to_ipv4_address_str(u32_to_bytes(netzwerk_id), ""), prfx));
             printf(format_str("Broadcast IP: %s\n", bytes_to_ipv4_address_str(u32_to_bytes(broadcast_ip), "")));
 
             printf(format_str("Min Usable IP: %s\n", prfx_diff < 2 ? "NA" : bytes_to_ipv4_address_str(u32_to_bytes(min_ip), "")));
